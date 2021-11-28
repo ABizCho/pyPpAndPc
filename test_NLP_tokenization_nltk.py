37d83a17c17effb8b8e81c82b94ef6a8f51a6a07
seongwoo <he1236@ajou.ac.kr>
2021-11-29 01:54:43 +0900

    Testing tokenization of session keyword

--- test_NLP_tokenization_nltk.py
+++ test_NLP_tokenization_nltk.py
@@ -1,20 +1,83 @@
-#from nltk.tokenize import word_tokenize
+# #from nltk.tokenize import word_tokenize
 
 
-#모든 사용자입력 session keyword는 소문자로 작성돼야한다. : yake로 선정된 ai원고 연관키워드와의 비교에 사용되기 때문에 소문자로 통일시킨다.
+# #모든 사용자입력 session keyword는 소문자로 작성돼야한다. : yake로 선정된 ai원고 연관키워드와의 비교에 사용되기 때문에 소문자로 통일시킨다.
 
-from nltk.tokenize import TreebankWordTokenizer
-tokenizer = TreebankWordTokenizer()
-
-
-sentence = "Rockstar, Hype, Monster and Redbull are four of the most popular energy drink brands in the world. These drinks are often consumed by athletes and people who work for long hours to provide them with a boost of energy. Rockstar: Rockstar is one of the most popular energy drinks in the world. It has been endorsed by many famous artists such as Lil Wayne and Macklemore. It also sponsors many sporting events such as Ironman, NASCAR and Indycar races. Monster: Monster is one of the most popular energy drinks in the world with their slogan “Unleash The Beast” which is targeted at high-performance athletes. It also sponsors many sporting events such as Ironman, NASCAR and Indycar races. Rockstar energy drink is known for its high caffeinated content. It also contains taurine, guarana extract, and B vitamins. Hype energy drink is a low cost product with low sugar content. It also contains L-carnitine and ginseng extract. Monster energy drink is an iconic brand, famous for its catchy slogans like ‘Unleash the Beast’ and ‘Unleash your Inner Monster’. The ingredients of this product include glucose syrup, sodium citrate, sucrose syrup, carbonated water (carbon dioxide), natural & artificial flavors (contains milk), vitamin C (Ascorbic Acid), flavoring (contains milk), caffeine from guarana extract and natural flavors (contains milk)."
-
-#구두점 제거
-cleaned_sentence = sentence.replace('!', '').replace(',','').replace('.','').replace('“','').replace('”','').replace('\n','').replace('’','').replace(':','').replace('&','').replace('(','').replace(')','').replace('[','').replace(']','').replace("'",'').replace('‘','')
-
-#명사 추출
-
-print(word_tokenize(cleaned_sentence))
+# from nltk.tokenize import TreebankWordTokenizer
+# tokenizer = TreebankWordTokenizer()
 
 
+# #계획 : process1의 사용자 입력에서 등장한 'session keyword : 합성어 키워드'를 아래와 같이 분해시킬 예정.
+#     # 기존의 분해되기 전 session keywords와 아래에서 분해된 개별 단어들을 모두 comparedKeywordList에 저장한다.
+#     # + 최소단위로 분해시킨 각 word를 위에 저장된 키워드를 제외시키고 모든경우의 수로 조합하여, comparedwordList에 저장한다.
+#     # yake를 이용해서 proc2:rytr의 결과물인 ai원고에서 파생시킬 연관키워드를 선정할 때, comparedkeywordList와 비교하여 저장한다.
+# def makeComparedKeywordList(sessionKwordList) :
+#     comparedKwordList = []
+    
+#     for i in range(5) :
+#         comparedKwordList.extend(tokenizer.tokenize(sessionKwordList[i]))
+#     print(comparedKwordList)
+    
+#     return comparedKwordList #메인 process에서 comparedKeywordList로 받게 될 예정 
 
+
+# ##구두점 제거
+# #cleaned_sentence = sentence.replace('!', '').replace(',','').replace('.','').replace('“','').replace('”','').replace('\n','').replace('’','').replace(':','').replace('&','').#replace('(','').replace(')','').replace('[','').replace(']','').replace("'",'').replace('‘','')
+
+
+# # #Tokenization
+# # print(tokenizer.tokenize(sentence))
+# # #print(word_tokenize(cleaned_sentence))
+
+# #Test makeComparedKeywordList
+# Test_inputKwordList = [] #사용자에게서 받는 모든 session keyword는 소문자로 작성하거나 / 소문자로 변경해야한다. : 파생원고로부터 연관키워드 선정 시 비교=>중복제거 수행예정
+# for i in range(5) :
+#     Test_inputKwordList.append(input('Type Your 5 keywords : '))
+
+# Test_inputKwordList = "{0}, {1}, {2}, {3}, {4}".format(Test_inputKwordList[0],Test_inputKwordList[1],Test_inputKwordList[2],Test_inputKwordList[3],Test_inputKwordList[4])
+# print(Test_inputKwordList)
+# print(Test_inputKwordList)
+
+# makeComparedKeywordList(Test_inputKwordList)
+
+
+
+#+===================
+
+
+import yake
+kw_extractor = yake.KeywordExtractor()
+
+language ="en"
+max_ngram_size = 3
+deduplication_threshold = 0.9
+numOfKeywords = 20
+custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords,features=None)
+# keywords = custom_kw_extractor.extract_keywords('script')
+# for kw in keywords:
+#     print(kw)
+
+
+#def
+def makeComparedKeywordList(sessionKwordList) : #input은 main의 sessionKeywordList가 들어갈 예정 (not var 'sessionKeyword')
+    comparedKwordList = list(range(len(sessionKwordList)))
+    
+    for skw in range(len(sessionKwordList)) :
+        keywords = custom_kw_extractor.extract_keywords(sessionKwordList[skw])
+        print(keywords)
+        keywordList = list()
+        
+        for i in range(len(keywords)) :
+            keywordList.append(keywords[i][0])
+        comparedKwordList.extend(keywordList)
+
+    return comparedKwordList
+
+#test_input
+Test_inputKwordList = []
+for i in range(5) :
+    Test_inputKwordList.append(input('Type Your 5 keywords : '))
+
+#test
+testresult = makeComparedKeywordList(Test_inputKwordList)
+print(testresult)
